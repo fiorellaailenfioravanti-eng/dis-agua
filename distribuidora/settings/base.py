679a55be17c354a6211014d7f23d34d46a519acd
fiorellaailenfioravanti-eng <fiorellaailenfioravanti@gmail.com>
2025-12-19 22:33:29 -0300

    Primera app

--- distribuidora/settings/base.py
+++ distribuidora/settings/base.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.productos',
+    'apps.usuarios',
 ]
 
 MIDDLEWARE = [
